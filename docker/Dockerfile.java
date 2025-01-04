7704600904df7222af2dc1a362d2ba7a53e12f4e
Peter Edwards <Techdread@gmail.com>
2025-01-04 14:03:00 +0000

    docker java

--- docker/Dockerfile.java
+++ docker/Dockerfile.java
@@ -12,7 +12,7 @@ RUN useradd -m -u 1001 coderunner && \
     chown -R coderunner:coderunner /app
 
 # Copy and compile the runner file
-COPY --chown=coderunner:coderunner docker/Runner.java /app/
+COPY --chown=coderunner:coderunner Runner.java /app/
 RUN javac Runner.java
 
 # Switch to non-root user
